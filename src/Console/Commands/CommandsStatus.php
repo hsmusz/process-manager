caa8d7793bfef84fe23ebc912ac1b1c2d3c0fea1
Hubert Smusz <hubert.smusz@movecloser.pl>
2025-12-02 19:07:11 +0100

    fixed commands status CLI display formating

--- src/Console/Commands/CommandsStatus.php
+++ src/Console/Commands/CommandsStatus.php
@@ -7,7 +7,6 @@ namespace Movecloser\ProcessManager\Console\Commands;
 use Illuminate\Console\Command;
 use Movecloser\ProcessManager\Lockdown\CommandLock;
 use Movecloser\ProcessManager\Lockdown\CommandStatusResolver;
-use Symfony\Component\Console\Formatter\OutputFormatterStyle;
 
 class CommandsStatus extends Command
 {
@@ -19,28 +18,48 @@ class CommandsStatus extends Command
      */
     public function handle(): void
     {
-        $this->info(sprintf('1. All commands: %s', CommandLock::allCommandsDisabled() ? 'DISABLED' : 'Enabled'));
-        $this->info(sprintf('2. Process Manager: %s', CommandStatusResolver::checkCommandStatus(ProcessManager::lockKey())));
-        $style = new OutputFormatterStyle('yellow');
-        $this->output->getFormatter()->setStyle('warning', $style);
+        $this->displaySystemStatus();
+        $this->displayRegisteredCommands();
+    }
 
+    private function displayRegisteredCommands(): void
+    {
         $commands = CommandStatusResolver::commands();
+
         if (empty($commands)) {
             return;
         }
 
         $this->info('3. Registered commands:');
 
-        foreach ($commands as $command => $title) {
-            if (is_array($title)) {
-                [$title, $params] = $title;
-            } else {
-                $params = [null];
+        foreach ($commands as $commandClass => $config) {
+            $this->renderCommandStatus($commandClass, $config);
+        }
+    }
+
+    private function displaySystemStatus(): void
+    {
+        $allDisabled = CommandLock::allCommandsDisabled() ? 'DISABLED' : 'Enabled';
+        $this->info(sprintf('1. All commands: %s', $allDisabled));
+
+        $managerStatus = CommandStatusResolver::checkCommandStatus(ProcessManager::lockKey());
+        $this->info(sprintf('2. Process Manager: %s', $managerStatus));
+    }
+
+    private function renderCommandStatus(string $commandClass, string|array $config): void
+    {
+        [$title, $params] = is_array($config) ? $config : [$config, [null]];
+
+        foreach ($params as $param) {
+            if (!empty($param)) {
+                $param = (string) $param;
             }
 
-            foreach ($params as $param) {
-                $this->info(sprintf("     %s\t %s", str_pad($title, 40), CommandStatusResolver::checkCommandStatus($command::lockKey($param))));
-            }
+            $lockKey = $commandClass::lockKey($param);
+            $status = CommandStatusResolver::checkCommandStatus($lockKey);
+            $suffix = $param ? " ($param)" : '';
+
+            $this->warn(sprintf('     %s%s: %s', $title, $suffix, $status));
         }
     }
 }
